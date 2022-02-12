163660bdadc14f759981563a38933e0c8277ed20
aiocat <aiocat@protonmail.com>
2022-02-12 19:25:50 +0300

    cli file argument

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
+import "os"
+
 func main() {
+	if len(os.Args) < 2 {
+		panic("File must be given")
+	}
+
+	fileName := os.Args[1]
+
 	compiler := new(Compiler)
-	compiler.FileName = "./test/test.c"
+	compiler.FileName = fileName
 
 	compiler.GetSource()
 	compiler.GetCompiler()
